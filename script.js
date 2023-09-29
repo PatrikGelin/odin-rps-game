19c4e5ec50a14ca030fc4c06350dc7a9066b94e9
Patrik Gelin <info@patrikgelin.se>
2023-09-29 11:51:42 +0200

    Add playRound function in script.js and comments to getComputerChoice function

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    // array of choices
+    const choices = ["rock", "paper", "scissors"];
+    // randoms a number between 0-2 (elements in the array)
     const random = Math.floor(Math.random() * choices.length);
+    // return the corresponding element of that number
     return choices[random];
 }
-console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    let pS = playerSelection.toLowerCase();
+    let cS = computerSelection;
+
+    if ( pS === cS) {
+        return "Draw!"
+    } else if (pS === "rock") {
+        if (cS === "scissors") {
+            return "You Win! Rock beats Scissors";
+        } else if (cS === "paper") {
+            return "You Lose! Paper Beats Rock";
+        }
+    } else if (pS === "paper") {
+        if (cS === "rock") {
+            return "You Win! Paper beats Rock";
+        } else if (cS === "scissors") {
+            return "You Lose! Scissors Beats Paper";
+        }
+    } else if (pS === "scissors") {
+        if (cS === "paper") {
+            return "You Win! Scissors beats Paper";
+        } else if (cS === "rock") {
+            return "You Lose! Rock Beats Scissors";
+        }
+    }
+}
+
+const playerSelection = "PAPER";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
