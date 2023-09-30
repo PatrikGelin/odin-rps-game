710ebba9087a54ebc55870dd563838cfa951c6ca
Patrik Gelin <info@patrikgelin.se>
2023-09-30 13:29:03 +0200

    Add comments to game function and playRound function

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // shorten the variable names and make sure that player input is lowercase
     let pS = playerSelection.toLowerCase();
     let cS = computerSelection;
 
+    // logic to figure out who won
     if ( pS === cS) {
         return "Draw!"
     } else if (pS === "rock") {
@@ -35,24 +37,31 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-
+    // variables to keep track of the score during a 5 game match
     let pScore = 0;
     let cScore = 0;
 
+    // for loop that will run 5 games
     for (let i=0; i<5; i++) {
+        // prompt the player for input
         let pS = prompt("Chose your weapon! Rock, Paper or Scissors");
+        // get computers choice 
         let cS = getComputerChoice();
+        // play the round 
         let round = playRound(pS, cS);
+        // check the 5th letter in the return value
         let winner = round.slice(4, 5);
-
+        // if the 5th letter is a W then its a win for the player, add 1 to player score
         if (winner === "W") {
             pScore ++;
+        // if its a L then add 1 to computer score 
         } else if (winner === "L") {
             cScore ++;
-        }
+        } // else dont add score bc its a tie
+
         console.log(round);
     }
-
+    // after the 5 rounds check scoring and log the winner
     if (pScore === cScore) {
         console.log("Game over! its a Draw!");
     } else if (pScore > cScore) {  
@@ -61,5 +70,5 @@ function game() {
         console.log("Game over! You lose!");
     }
 }
-
+// run a new game on page refresh
 game();
